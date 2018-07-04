5027b1f43304c89bfc19858136621049b08d61e4
Ambler <allinlbj@gmail.com>
2018-07-04 10:45:48 +0800

    demo高亮

--- js/main.js
+++ js/main.js
@@ -4,8 +4,31 @@ var result = `/*
  * 我就用代码来介绍吧
  * 首先准备一些样式
  */
+
+* {
+    transition: all 1s;
+}
 body {
-    background: red;
+    background: rgb(222,222,222);
+    font-size: 16px;
+}
+#code {
+    border: 1px solid red;
+    padding: 16px;
+}
+
+/* 我需要一点代码高亮 */
+.token.selector {
+    color: #690;
+}
+.token.punctuation {
+    color: #999;
+}
+.token.property {
+    color: #905;
+}
+.token.function {
+    color: #DD4A68;
 }
 `
 
@@ -13,9 +36,10 @@ var n = 0
 var id = setInterval(()=> {
     n += 1
     code.innerHTML = result.substring(0,n)
+    code.innerHTML = Prism.highlight(code.innerHTML, Prism.languages.css, 'css')
     styleTag.innerHTML = result.substring(0,n)
     console.log(`第${n}轮`)
     if( n >= result.length) {
         window.clearInterval(id)
     }
-},100)
+},10)
