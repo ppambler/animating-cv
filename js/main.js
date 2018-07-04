6f1f7dc78193f0d4c2c8681b9bf78e85441000f9
Ambler <allinlbj@gmail.com>
2018-07-04 13:14:06 +0800

    「添加一张白纸」

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ body {
 }
 #code {
     border: 1px solid red;
-    padding: 16px;
+    padding: 16px; 
 }
 
 /* 我需要一点代码高亮 */
@@ -30,16 +30,55 @@ body {
 .token.function {
     color: #DD4A68;
 }
+
+/* 加点3D效果 */
+#code {
+    transform: rotate(360deg);
+}
+
+/* 不玩了，我来介绍一下我自己吧 */
+/* 我需要一张白纸 */
 `
 
 var n = 0
-var id = setInterval(()=> {
+var id = setInterval(() => {
     n += 1
-    code.innerHTML = result.substring(0,n)
+    code.innerHTML = result.substring(0, n)
     code.innerHTML = Prism.highlight(code.innerHTML, Prism.languages.css, 'css')
-    styleTag.innerHTML = result.substring(0,n)
+    styleTag.innerHTML = result.substring(0, n)
     console.log(`第${n}轮`)
-    if( n >= result.length) {
+    if (n >= result.length) {
         window.clearInterval(id)
+        // 结束了，就开始执行第二个程序
+        fn2()
+        fn3(result)
     }
-},10)
+}, 10)
+
+function fn2() {
+    var paper = document.createElement('div')
+    paper.id = 'paper'
+    document.body.appendChild(paper)
+}
+
+function fn3(preResult) {
+    var result = `#paper {
+    width: 100px;
+    height: 100px;
+    background: red;
+}
+    `
+    var n = 0
+    var id = setInterval(() => {
+        n += 1
+        // 加上之前的结果很是关键，有个疑问？？每次都得刷新一大段文本？？
+        // 没有吧？我的肉眼没有看到颜色一瞬间的变化
+        code.innerHTML = preResult + result.substring(0,n)
+        code.innerHTML = Prism.highlight(code.innerHTML, Prism.languages.css, 'css')
+        styleTag.innerHTML += result.substring(n-1, n)
+        console.log(`第${n}轮`)
+        if (n >= result.length) {
+            window.clearInterval(id)
+        }
+    }, 10)
+}
