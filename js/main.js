513bc8dbb75baf84475143fb4bedf668f05f23e6
Ambler <allinlbj@gmail.com>
2018-07-05 10:07:36 +0800

    「处理创建一个paper后封装demo的后遗症」

--- js/main.js
+++ js/main.js
@@ -40,14 +40,14 @@ body {
 /* 我需要一张白纸 */
 `
 // 把code写到#code和style标签里去
-function writeCode(code,preTag,styleTag,createPaper) {
+function writeCode(prefix,code,preTag,styleTag,createPaper) {
     let domPreTag = document.querySelector(preTag)
     let domStyleTag = document.querySelector(styleTag)
     let n = 0
     let id = setInterval(() => {
         n += 1
-        domPreTag.innerHTML = Prism.highlight(code.substring(0, n), Prism.languages.css, 'css')
-        domStyleTag.innerHTML = code.substring(0, n)
+        domPreTag.innerHTML = Prism.highlight(prefix+code.substring(0, n), Prism.languages.css, 'css')
+        domStyleTag.innerHTML =prefix+ code.substring(0, n)
         console.log(`输出了${n}字符`)
         if (n >= result.length) {
             window.clearInterval(id)
@@ -56,32 +56,22 @@ function writeCode(code,preTag,styleTag,createPaper) {
     }, 10)
 }
 
-writeCode(result,'#code','#styleTag',createPaper)
+writeCode('',result,'#code','#styleTag',()=>{
+    createPaper(()=>{
+        writeCode(result,result2,'#code','#styleTag')
+    })
+})
 
-function createPaper() {
-    var paper = document.createElement('div')
-    paper.id = 'paper'
-    document.body.appendChild(paper)
-}
-
-function fn3(preResult) {
-    var result = `#paper {
+var result2 = `#paper {
     width: 100px;
     height: 100px;
     background: red;
 }
     `
-    var n = 0
-    var id = setInterval(() => {
-        n += 1
-        // 加上之前的结果很是关键，有个疑问？？每次都得刷新一大段文本？？
-        // 没有吧？我的肉眼没有看到颜色一瞬间的变化
-        code.innerHTML = preResult + result.substring(0,n)
-        code.innerHTML = Prism.highlight(code.innerHTML, Prism.languages.css, 'css')
-        styleTag.innerHTML += result.substring(n-1, n)
-        console.log(`第${n}轮`)
-        if (n >= result.length) {
-            window.clearInterval(id)
-        }
-    }, 10)
-}
+function createPaper(xxx) {
+    var paper = document.createElement('div')
+    paper.id = 'paper'
+    document.body.appendChild(paper)
+    xxx()
+}  
+
